cc7425b11c37aa10dd05ee0381f2156c5a62673a
CarlLevillain <91659706+CarlLevillain@users.noreply.github.com>
2023-11-22 16:22:54 +0100

    tableau js bonus penalty

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ spinBtn.onclick = function(){
 
     value += Math.ceil(Math.random() * 3600);
 }
+
+let penalty = [
+    '-15 seconds on the timer for the other team',
+    'The other team do only use mimics ',
+    'During the round the speaker of the other team do squats / chair ',
+    'The other team say just 3 words per cards',
+    'The other team can’t past cards during the next round',
+    'The other team can say only one answer'
+];
+
+let bonus =[
+    '+15 sec on the timer for your team',
+    'Your team can use one French word ',
+    '+2 point for your team',
+    'You gain 5 sec par card this effect will be activated for the next round'
+]
