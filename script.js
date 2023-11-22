af1b36c7eecbbea47187131970cfc05b9ed83c5e
CarlLevillain <91659706+CarlLevillain@users.noreply.github.com>
2023-11-22 16:53:04 +0100

    test roulette

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ let wheel = document.querySelector('.wheel');
 let spinBtn = document.querySelector('.spinBtn');
 let value = Math.ceil(Math.random() * 3600);
 
-spinBtn.onclick = function(){
-    wheel.style.transform = "rotate(" + value + "deg)"
+var numberOfOptions = 8; // Nombre total de cases sur la roulette
 
-    value += Math.ceil(Math.random() * 3600);
-}
+spinBtn.onclick = function(){
+    var value = Math.ceil(Math.random() * 3600);
+    wheel.style.transform = "rotate(" + value + "deg)";
+
+    setTimeout(function() {
+        var degrees = value % 360; // Obtenez l'angle réel après la rotation
+        var segmentSize = 360 / numberOfOptions; // Taille d'un segment en degrés
+        var winningSegmentIndex = Math.floor((360 - degrees) / segmentSize);
+
+        // Récupérez le contenu de la case gagnante
+        var winningContent = document.querySelectorAll('.number')[winningSegmentIndex].textContent;
+        console.log(winningContent); // Affichez le contenu de la case gagnante
+    }, 5000); // Assurez-vous que ce délai correspond à la durée de l'animation
+};
 
 let penalty = [
     '-15 seconds on the timer for the other team',
@@ -22,5 +33,5 @@ let bonus =[
     'Your team can use one French word ',
     '+2 point for your team',
     'You gain 5 sec par card this effect will be activated for the next round'
-]
+];
 
