89ac939f73e95b682bea5e0d185e05d8875a0bc0
CarlLevillain <91659706+CarlLevillain@users.noreply.github.com>
2023-11-22 14:09:58 +0100

    centrage wheel

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let spinBtn = document.querySelector('.spinBtn');
 let value = Math.ceil(Math.random() * 3600);
 
 spinBtn.onclick = function(){
-    wheel.style.transform = "rotate(" + 99999999999999999999999999999999999999999999 + "deg)"
+    wheel.style.transform = "rotate(" + value + "deg)"
 
     value += Math.ceil(Math.random() * 3600);
 }
